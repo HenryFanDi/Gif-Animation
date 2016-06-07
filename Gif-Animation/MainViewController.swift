9bd3048bcca9ffb259ced56c09f99fbcae5df83f
Henry Fan <henry.fan@centraltrillion.com>
2016-06-07 21:59:36 +0800

    Initialize Project

---
+++ Gif-Animation/MainViewController.swift
@@ -0,0 +1,23 @@
+//
+//  MainViewController.swift
+//  Gif-Animation
+//
+//  Created by HenryFan on 7/6/2016.
+//  Copyright © 2016 HenryFanDi. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+  
+  // MARK: Lifecycle
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+  }
+  
+}
