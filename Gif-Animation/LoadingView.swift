d09433e225517cf1f00bcbd305fa301974b44306
Henry Fan <henry.fan@centraltrillion.com>
2016-06-10 08:19:12 +0800

    feat: adjust background view and add loading view

--- Gif-Animation/LoadingView.swift
+++ Gif-Animation/LoadingView.swift
@@ -19,14 +19,31 @@ extension UIViewController {
     get {
       var view = objc_getAssociatedObject(self, &backgroundViewKey) as? UIView
       if view == nil {
-        view = UIView.init()
-        view?.backgroundColor = UIColor.blackColor()
+        view = UIView.init(frame: CGRectMake(0.0, 0.0, CGRectGetWidth(UIScreen.mainScreen().bounds), CGRectGetHeight(UIScreen.mainScreen().bounds)))
+        view?.backgroundColor = UIColor.blackColor().colorWithAlphaComponent(0.4)
         self.backgroundView = view
       }
       return view
     }
     set(newValue) {
       objc_setAssociatedObject(self, &backgroundViewKey, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+    }
+  }
+  
+  var loadingView: UIView? {
+    get {
+      var view = objc_getAssociatedObject(self, &loadingViewKey) as? UIView
+      if view == nil {
+        view = UIView.init(frame: CGRectMake(0.0, 0.0, 204.0, 167.0))
+        view?.backgroundColor = UIColor.whiteColor().colorWithAlphaComponent(0.8)
+        view?.center = CGPointMake(CGRectGetWidth(UIScreen.mainScreen().bounds) / 2.0, CGRectGetHeight(UIScreen.mainScreen().bounds) / 2.0)
+        view?.layer.cornerRadius = 13.0
+        self.loadingView = view
+      }
+      return nil
+    }
+    set(newValue) {
+      objc_setAssociatedObject(self, &loadingViewKey, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
     }
   }
   
